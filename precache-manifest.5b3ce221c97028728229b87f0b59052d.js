97ba09c59a99e51a14bfe1f96131c7a29d8bfd97
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-04-22 02:05:51 +0700

    Updates

---
+++ precache-manifest.5b3ce221c97028728229b87f0b59052d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016cf97556fa8592b3125b2287a74a03",
+    "url": "/gastifrontend/index.html"
+  },
+  {
+    "revision": "0f77bf77d0a5106a7822",
+    "url": "/gastifrontend/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7d0f86f69b3fd5204e40",
+    "url": "/gastifrontend/static/css/main.4260d0bb.chunk.css"
+  },
+  {
+    "revision": "0f77bf77d0a5106a7822",
+    "url": "/gastifrontend/static/js/2.b1eb188a.chunk.js"
+  },
+  {
+    "revision": "81c8e4b533017ac7156eb226b76c1e7f",
+    "url": "/gastifrontend/static/js/2.b1eb188a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0f86f69b3fd5204e40",
+    "url": "/gastifrontend/static/js/main.89cecf56.chunk.js"
+  },
+  {
+    "revision": "638df3222ad7b0717ee0",
+    "url": "/gastifrontend/static/js/runtime-main.590f35cc.js"
+  },
+  {
+    "revision": "279013c3fac25b79ef7abab3e8a0c72c",
+    "url": "/gastifrontend/static/media/bg-03.279013c3.png"
+  },
+  {
+    "revision": "d8535bfa87db3a4b6114c70d63592bbf",
+    "url": "/gastifrontend/static/media/img_peta.d8535bfa.png"
+  },
+  {
+    "revision": "8d38768e0460ddee46c3b2a8af2955b5",
+    "url": "/gastifrontend/static/media/segitiga-02.8d38768e.png"
+  },
+  {
+    "revision": "2e47d106ca7d1ec63a275bde820ab9ac",
+    "url": "/gastifrontend/static/media/segitiga-03.2e47d106.png"
+  },
+  {
+    "revision": "fbff154525a4f0388cc5ff2296be4c1b",
+    "url": "/gastifrontend/static/media/wh-example.fbff1545.png"
+  }
+]);
