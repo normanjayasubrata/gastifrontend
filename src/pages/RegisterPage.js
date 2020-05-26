3414c6a61027318eb7e8b7c9c477ad21a57c3dce
Richard <richard.kartiyanta@icehousecorp.com>
2020-05-26 17:56:49 +0700

    Add post request to register user

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,264 +1,273 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
 import { Link } from "react-router-dom";
-
 import { Form, Button, Container, Col, Row } from "react-bootstrap";
-
+import axios from 'axios';
 
 export class RegisterPage extends Component {
-         state = {
-           form: {
-             fullname: "",
-             email: "",
-             password: "",
-             phoneNumber: "",
-             address: "",
-             province: "",
-             city: "",
-             district: "",
-             subdistrict: "",
-             zipcode: "",
-           },
-           isAgree: false,
-           isAllValidated: false,
-         };
-         onChangeHandler = (event) => {
-           const { name, value, checked } = event.target;
+  state = {
+    form: {
+      fullname: "",
+      email: "",
+      password: "",
+      phoneNumber: "",
+      address: "",
+      province: "",
+      city: "",
+      district: "",
+      subdistrict: "",
+      zipcode: "",
+    },
+    isAgree: false,
+    isAllValidated: false,
+  };
 
-           this.setState((state) => {
-             if (name === "checkbox") {
-               return { isAgree: checked };
-             } else {
-               return {
-                 form: {
-                   ...state.form,
-                   [name]: value,
-                 },
-               };
-             }
-           });
-           setTimeout(() => {
-             this.validateAll();
-           }, 100);
-         };
+  onChangeHandler = (event) => {
+    const { name, value, checked } = event.target;
 
-         validateAll = () => {
-             console.log(this.state.form)
-             let validfullname = this.state.form.fullname.trim() !== "";
-             let validemail = this.state.form.email.trim() !== "";
-             let validpassword = this.state.form.password.trim() !== "";
-           let validPhoneNumber = !isNaN(this.state.form.phoneNumber) && this.state.form.phoneNumber.trim() !== "";
-           let validaddress = this.state.form.address.trim() !== "";
-           let validprovince = this.state.form.province.trim() !== "";
-           let validcity = this.state.form.city.trim() !== "";
-           let validdistrict = this.state.form.district.trim() !== "";
-           let validsubdistrict = this.state.form.subdistrict.trim() !== "";
-           let validzipcode = this.state.form.zipcode.trim() !== "";
+    this.setState((state) => {
+      if (name === "checkbox") {
+        return { isAgree: checked };
+      } else {
+        return {
+          form: {
+            ...state.form,
+            [name]: value,
+          },
+        };
+      }
+    });
+    setTimeout(() => {
+      this.validateAll();
+    }, 100);
+  };
+
+  validateAll = () => {
+    console.log(this.state.form)
+    let validfullname = this.state.form.fullname.trim() !== "";
+    let validemail = this.state.form.email.trim() !== "";
+    let validpassword = this.state.form.password.trim() !== "";
+    let validPhoneNumber = !isNaN(this.state.form.phoneNumber) && this.state.form.phoneNumber.trim() !== "";
+    let validaddress = this.state.form.address.trim() !== "";
+    let validprovince = this.state.form.province.trim() !== "";
+    let validcity = this.state.form.city.trim() !== "";
+    let validdistrict = this.state.form.district.trim() !== "";
+    let validsubdistrict = this.state.form.subdistrict.trim() !== "";
+    let validzipcode = this.state.form.zipcode.trim() !== "";
 
 
-           if ( validfullname && validemail && validpassword && validPhoneNumber && validaddress && validprovince && validcity && validdistrict && validsubdistrict && validzipcode && this.state.isAgree ) {
-             this.setState({ isAllValidated: true });
-           } else {
-             this.setState({ isAllValidated: false });
-           }
-         };
+    if (validfullname && validemail && validpassword && validPhoneNumber && validaddress && validprovince && validcity && validdistrict && validsubdistrict && validzipcode && this.state.isAgree) {
+      this.setState({ isAllValidated: true });
+    } else {
+      this.setState({ isAllValidated: false });
+    }
+  };
 
-         onSubmitHandler = (event) => {
-           event.preventDefault();
-           alert(
-             `
-                {
-                    "account_type": "${this.props.accountType.name}",
-                    "account_role": "ADMIN",
-                    "auth": {
-                        "username": "${this.state.form.phoneNumber}",
-                        "password": "${this.state.form.password}"
-                      },
-                    "detail": {
-                      "fullname": "${this.state.form.fullname}",
-                      "address": "${this.state.form.address}",
-                      "email": "${this.state.form.email}",
-                      "province": "${this.state.form.province}",
-                      "city": "${this.state.form.city}",
-                      "district": "${this.state.form.district}",
-                      "sub_district": "${this.state.form.subdistrict}",
-                      "zipcode": ${this.state.form.zipcode},
-                      "bank_id": "BCA",
-                      "bank_account": "1234567890"
-                    }
-                  }
-                `
-           );
-         };
-         render() {
-           // console.log(this.props)
-           const { isNotMobile } = this.props;
-           const paddingLefRight = isNotMobile ? "20rem" : "1rem";
-           const inactiveButton = (
-             <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "#00000029", color: "#909090", border: "none", height: "3rem", }} disabled >
-               Daftar
-             </Button>
-           );
-           const activeButton = (
-             <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "#DF1C78", border: "none", height: "3rem", }} >
-               Daftar
-             </Button>
-           );
-           // console.log(this.state)
-           return (
-             <div
-               style={{
-                 paddingTop: isNotMobile ? "2rem" : "1rem",
-                 paddingBottom: isNotMobile ? "4rem" : "2rem",
-               }}
-             >
-               <Container>
-                 <Col>
-                   <Row className="justify-content-center">
-                     <h1 style={{ fontWeight: "300" }}>Selamat Datang</h1>
-                   </Row>
-                   <Row
-                     className="justify-content-center"
-                     style={{
-                       borderBottom: "solid #00000029 1px",
-                       marginRight: paddingLefRight,
-                       marginLeft: paddingLefRight,
-                       marginBottom: "2rem",
-                     }}
-                   >
-                     <span>
-                       <p>Sudah punya akun?</p>
-                     </span>
-                     <span>
-                       <Link to="/Login">
-                         <p style={{ fontWeight: "bold", color: "#DF1C78" }}>
-                           Masuk
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'a'
+      }
+    }
+
+    let data = JSON.stringify({
+      account_type: 'GUDANG',
+      account_role: 'ADMIN',
+      auth: {
+        username: this.state.form.phoneNumber,
+        password: this.state.form.password
+      },
+      detail: {
+        fullname: this.state.form.fullname,
+        address: this.state.form.address,
+        email: this.state.form.email,
+        province: this.state.form.province,
+        city: this.state.form.city,
+        district: this.state.form.district,
+        sub_district: this.state.form.subdistrict,
+        zipcode: parseInt(this.state.form.zipcode),
+        bank_id: "BCA",
+        bank_account: "1234567890"
+      }
+    })
+
+    axios.post('http://127.0.0.1:8088/v1/account/register', data, config).then((response) => {
+      alert(response);
+    }, (error) => {
+      alert(error);
+    });
+  };
+
+  render() {
+    const { isNotMobile } = this.props;
+    const paddingLefRight = isNotMobile ? "20rem" : "1rem";
+    const inactiveButton = (
+      <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "#00000029", color: "#909090", border: "none", height: "3rem", }} disabled >
+        Daftar
+      </Button>
+    );
+    const activeButton = (
+      <Button variant="primary" type="submit" style={{ width: "100%", backgroundColor: "#DF1C78", border: "none", height: "3rem", }} >
+        Daftar
+      </Button>
+    );
+
+    return (
+      <div
+        style={{
+          paddingTop: isNotMobile ? "2rem" : "1rem",
+          paddingBottom: isNotMobile ? "4rem" : "2rem",
+        }}
+      >
+        <Container>
+          <Col>
+            <Row className="justify-content-center">
+              <h1 style={{ fontWeight: "300" }}>Selamat Datang</h1>
+            </Row>
+            <Row
+              className="justify-content-center"
+              style={{
+                borderBottom: "solid #00000029 1px",
+                marginRight: paddingLefRight,
+                marginLeft: paddingLefRight,
+                marginBottom: "2rem",
+              }}
+            >
+              <span>
+                <p>Sudah punya akun?</p>
+              </span>
+              <span>
+                <Link to="/Login">
+                  <p style={{ fontWeight: "bold", color: "#DF1C78" }}>
+                    Masuk
                          </p>
-                       </Link>
-                     </span>
-                   </Row>
-                   <Row className="justify-content-center">
-                     <Form
-                       style={{ width: "27rem" }}
-                       onSubmit={this.onSubmitHandler}
-                     >
-                       <Form.Group>
-                         <Form.Label>Nama Lengkap</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="fullname" />
-                       </Form.Group>
+                </Link>
+              </span>
+            </Row>
+            <Row className="justify-content-center">
+              <Form
+                style={{ width: "27rem" }}
+                onSubmit={this.onSubmitHandler}
+              >
+                <Form.Group>
+                  <Form.Label>Nama Lengkap</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="fullname" />
+                </Form.Group>
 
-                       <Form.Group controlId="formBasicEmail">
-                         <Form.Label>Email address</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="email" />
-                         <Form.Text className="text-muted">
-                           Contoh: gudang@gasti.com
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="email" />
+                  <Form.Text className="text-muted">
+                    Contoh: gudang@gasti.com
                          </Form.Text>
-                       </Form.Group>
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Password</Form.Label>
-                         <Form.Control
-                           type="password"
-                           name="password"
-                           onChange={this.onChangeHandler}
-                           value={this.state.form.password}
-                         />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    onChange={this.onChangeHandler}
+                    value={this.state.form.password}
+                  />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Masukan No. HP</Form.Label>
-                         <Form.Control
-                           name="phoneNumber"
-                           onChange={this.onChangeHandler}
-                           style={
-                             isNaN(this.state.form.phoneNumber)
-                               ? { border: "solid red 2px" }
-                               : null
-                           }
-                           value={this.state.form.phoneNumber}
-                         />
-                         <Form.Text
-                           className={
-                             !isNaN(this.state.form.phoneNumber)
-                               ? "text-muted"
-                               : null
-                           }
-                           style={
-                             isNaN(this.state.form.phoneNumber)
-                               ? { color: "red" }
-                               : null
-                           }
-                         >
-                           {!isNaN(this.state.form.phoneNumber)
-                             ? "contoh: 081234567890"
-                             : "fill with number"}
-                         </Form.Text>
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Masukan No. HP</Form.Label>
+                  <Form.Control
+                    name="phoneNumber"
+                    onChange={this.onChangeHandler}
+                    style={
+                      isNaN(this.state.form.phoneNumber)
+                        ? { border: "solid red 2px" }
+                        : null
+                    }
+                    value={this.state.form.phoneNumber}
+                  />
+                  <Form.Text
+                    className={
+                      !isNaN(this.state.form.phoneNumber)
+                        ? "text-muted"
+                        : null
+                    }
+                    style={
+                      isNaN(this.state.form.phoneNumber)
+                        ? { color: "red" }
+                        : null
+                    }
+                  >
+                    {!isNaN(this.state.form.phoneNumber)
+                      ? "contoh: 081234567890"
+                      : "fill with number"}
+                  </Form.Text>
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Alamat Lengkap</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="address" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Alamat Lengkap</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="address" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Provinsi</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="province" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Provinsi</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="province" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Kota</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="city" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Kota</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="city" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Kecamatan</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="district" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Kecamatan</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="district" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Kelurahan</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="subdistrict" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Kelurahan</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="subdistrict" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Form.Label>Kode Pos</Form.Label>
-                         <Form.Control onChange={this.onChangeHandler} name="zipcode" />
-                       </Form.Group>
+                <Form.Group>
+                  <Form.Label>Kode Pos</Form.Label>
+                  <Form.Control onChange={this.onChangeHandler} name="zipcode" />
+                </Form.Group>
 
-                       <Form.Group>
-                         <Row className="justify-content-center">
-                           <Form.Check
-                             type="checkbox"
-                             name="checkbox"
-                             onChange={this.onChangeHandler}
-                           />
-                           <p>
-                             {" "}
-                             saya setuju dengan{" "}
-                             <Link to="/termcondition">syarat & ketentuan</Link>
-                           </p>
-                         </Row>
-                       </Form.Group>
-                       {this.state.isAllValidated
-                         ? activeButton
-                         : inactiveButton}
-                     </Form>
-                   </Row>
-                 </Col>
-               </Container>
-             </div>
-           );
-         }
-       }
+                <Form.Group>
+                  <Row className="justify-content-center">
+                    <Form.Check
+                      type="checkbox"
+                      name="checkbox"
+                      onChange={this.onChangeHandler}
+                    />
+                    <p>
+                      {" "}
+                      saya setuju dengan{" "}
+                      <Link to="/termcondition">syarat & ketentuan</Link>
+                    </p>
+                  </Row>
+                </Form.Group>
+                {this.state.isAllValidated
+                  ? activeButton
+                  : inactiveButton}
+              </Form>
+            </Row>
+          </Col>
+        </Container>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
-    ...state.windowSizeReducer, 
-    accountType: state.accountReducer.accountType
+  ...state.windowSizeReducer,
+  accountType: state.accountReducer.accountType
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegisterPage)
