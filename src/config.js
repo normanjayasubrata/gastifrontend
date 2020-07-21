d0a7e5ae2ba5c946eceaf571aa110a2642a71599
normanjayasubrata <norman.jayasubrata@gmail.com>
2020-07-21 18:33:10 +0700

    fix url

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
   API_BASE_URL: "https://private-12204a-gasti.apiary-mock.com",
-  // BASE_URL: "http://127.0.0.1:3000",
-  BASE_URL: "https://normanjayasubrata.github.io/gastifrontend",
+  BASE_URL: "/gastifrontend",
+  // BASE_URL: "https://normanjayasubrata.github.io/gastifrontend",
   REDIRECT_URL: "/",
   LOGIN_URL: "/login",
 };
